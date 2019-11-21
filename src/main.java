dd01e69c0f63a5e541f7d1d397cef384f4af814f
Jordan Ho <hojo21@up.edu>
2019-11-20 20:44:22 -0800

    created instance variables and readfile method

--- src/main.java
+++ src/main.java
@@ -5,10 +5,21 @@
 * Coding Project
 * Converting DFA to Regular Expression.
  */
-
 public class main {
+    // instance variables
+    private String formalDescription;
+    private String states;
+    private String alphabet;
+    private String caseDiagram;
+    private String startingState;
+    private String acceptingStates;
+
     public static void main(String args[]){
         System.out.println("Hello World");
     }
 
+    // reads the input file of the formal description and sorts data
+    public String readFile(String formalDescription){
+        return formalDescription;
+    }
 }
