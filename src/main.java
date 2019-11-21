50f318f1f17b15eac881ae0416a06a42340bcce3
marcial21 <marcial21@up.edu>
2019-11-20 20:25:02 -0800

    created main class and main method

---
+++ src/main.java
@@ -0,0 +1,14 @@
+/*
+* @author Gabe Marcial
+* @author Jordan Ho
+* CS 357
+* Coding Project
+* Converting DFA to Regular Expression.
+ */
+
+public class main {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+
+}
