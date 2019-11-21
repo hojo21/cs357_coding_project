55ba9326d50da9756d78adc5abd59e9f2d49c083
marcial21 <marcial21@up.edu>
2019-11-20 21:44:29 -0800

    created input text file and dfa class

---
+++ src/dfa.java
@@ -0,0 +1,23 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class dfa {
+    //Initialize start variables.
+    private ArrayList<String> states;
+    private ArrayList<Character> alphabet;
+    private String[][] delta;
+    private String start;
+    private ArrayList<String> acceptStates;
+
+    //Default constructor
+    public dfa(){
+
+    }
+    //input file constructor
+    public dfa(ArrayList<String> setOfStates, ArrayList<Character> sigma, String[][] transTable, String startState,
+               ArrayList<String> setOfAccept){
+
+    }
+
+}
