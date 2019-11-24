3bc048e9e39ed140333172297154ef6984e3253f
marcial21 <marcial21@up.edu>
2019-11-24 15:24:42 -0800

    properly proccesses information from file into instance variables except delta transition functions.

--- src/dfa.java
+++ src/dfa.java
@@ -1,23 +1,103 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class dfa {
     //Initialize start variables.
     private ArrayList<String> states;
-    private ArrayList<Character> alphabet;
+    private ArrayList<String> alphabet;
     private String[][] delta;
     private String start;
     private ArrayList<String> acceptStates;
 
     //Default constructor
     public dfa(){
-
     }
-    //input file constructor
+    //Parameter constructor constructor
     public dfa(ArrayList<String> setOfStates, ArrayList<Character> sigma, String[][] transTable, String startState,
                ArrayList<String> setOfAccept){
+        states = setOfStates;
 
+    }
+    //file constructor
+    public dfa(File dfa) throws FileNotFoundException {
+        /*Allocating instance variables*/
+        states = new ArrayList<String>();
+        alphabet = new ArrayList<String>();
+        acceptStates = new ArrayList<String>();
+
+        /*Processing information from the file*/
+        Scanner sc = new Scanner(dfa);
+        while(sc.hasNextLine()){
+            String line = sc.nextLine();
+            if(line.contentEquals("Q:")){
+                //System.out.print("Set of states.\n");
+                while(sc.hasNextLine()){
+                    String state = sc.nextLine();
+                    if(state.contentEquals("end")){
+                        break;
+                    }
+                    else states.add(state);
+                }
+            }
+            else if(line.contentEquals("Sigma:")){
+                //System.out.print("Sigma\n");
+                while(sc.hasNextLine()){
+                    String letter = sc.nextLine();
+                    if(letter.contentEquals("end")){
+                        break;
+                    }
+                    else alphabet.add(letter);
+                }
+            }
+            else if(line.contentEquals("Delta:")){
+                //System.out.print("Delta\n");
+            }
+            else if(line.contentEquals("Start:")){
+                //System.out.print("start state is \n");
+                while(sc.hasNextLine()){
+                    String startState = sc.nextLine();
+                    if(startState.contentEquals("end")){
+                        break;
+                    }
+                    else{
+                        start = startState;
+                    }
+                }
+            }
+            else if(line.contentEquals("F:")){
+                //System.out.print("Accept States are\n");
+                while(sc.hasNextLine()){
+                    String acceptState = sc.nextLine();
+                    if(acceptState.contentEquals("end")){
+                        break;
+                    }
+                    else{
+                        acceptStates.add(acceptState);
+                    }
+                }
+            }
+        }
+    }
+
+    /*Getters and Setters*/
+    public ArrayList<String> getStates(){
+        return this.states;
+    }
+
+    public ArrayList<String> getAlphabet(){
+        return this.alphabet;
+    }
+
+    public String getStart(){
+        return this.start;
+    }
+
+    public ArrayList<String> getAcceptStates(){
+        return this.acceptStates;
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /*
 * @author Gabe Marcial
 * @author Jordan Ho
@@ -14,8 +18,26 @@ public class main {
     private String startingState;
     private String acceptingStates;
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws FileNotFoundException {
         System.out.println("Hello World");
+        File myFile = new File("C:\\Users\\go4ma\\IdeaProjects\\cs357_coding_project\\src\\dfa.txt");
+        dfa myDFA = new dfa(myFile);
+        /*For debugging purposes*/
+        System.out.println("Set of states:");
+        for(String state : myDFA.getStates()){
+            System.out.println(state);
+        }
+        System.out.println("Alphabet:");
+        for(String letter : myDFA.getAlphabet()){
+            System.out.println(letter);
+        }
+        System.out.println("Start state: " + myDFA.getStart());
+        System.out.println("Delta table");
+        System.out.println("Accept States:");
+        for(String acceptState : myDFA.getAcceptStates()){
+            System.out.println(acceptState);
+        }
+
     }
 
     // reads the input file of the formal description and sorts data
